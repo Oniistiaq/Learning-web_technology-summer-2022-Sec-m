98863a7e48148a40610f4d80694fa3cf2356b9f2
Oniistiaq <106148406+Oniistiaq@users.noreply.github.com>
2022-06-07 17:00:45 +0600

    Add files via upload

---
+++ labo3.php
@@ -0,0 +1,10 @@
+<?php 
+$length=2;
+ $width  = 4;
+
+  $area =$lenth * $width;
+  $perimeter =2 * ( $length + $width );
+  echo " the area  is $area" ;
+    echo " the perimeter is $perimeter" ;
+  
+?>
